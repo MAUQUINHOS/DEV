5a13601adc355bb664ff9fd47baf394069ea9c2f
MAUQUINHOS <@mauquinhos>
2019-02-09 18:55:26 -0200

    Adicionando Funcoes e Variaveis de ambiente

--- parse_url.sh
+++ parse_url.sh
@@ -6,18 +6,17 @@
 # Autor : Marcos R. Pereira
 #
 # -----------------------------------------------------
-# Este programa recebe como um parametro um link, e após
+# Este programa recebe como um parametro um site e baixa sua index, e após
 # algumas etapas e passagens por filtros, retorna as URL's 
 # obtidas de dentro do link
 #
-#
-# Exemplo:
+# Exemplo de como utilizar:
 #
 #   $ ./parse_url.sh meusite.com.br
 #       
 #       Site: google.com
 #       HTTP_CODE: 301
-#       Urls obtidas: 15
+#       URL's obtidas: 15
 #       1 http://maps.google.com.br/maps?hl=pt-BR
 #       1 http://news.google.com.br/nwshp?hl=pt-BR
 #       1 https://accounts.google.com/ServiceLogin?hl=pt-BR
@@ -27,104 +26,50 @@
 #        accounts.google.com	:172.217.28.237 
 #        accounts.google.com	:mail 
 #       ...
-# -----------------------------------------------------
+#
+# Verifica os parametros existem para continuar o programa
+# Caso não tenha informa o modo de uso correto
+[ "$1" ] && {
 
-if [ "$1" == "" ];
-then
-clear
-echo "#############################"
-echo "Como utilizar:"
-echo -e "\t\t $0 www.site.com"
-echo "#############################"
-exit 1
-fi
-site=$(echo $1 | sed 's/https://' | sed 's/http://' | sed 's/www.//' | sed 's/\/\///' | cut -d "/" -f1)
-echo "Site: $site"
-code=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $site)
-err(){
-string="1xx Informational
-100 Continue
-101 Switching Protocols
-102 Processing (WebDAV)
-2xx Success
- 200 OK
- 201 Created
-202 Accepted
-203 Non-Authoritative Information
- 204 No Content
-205 Reset Content
-206 Partial Content
-207 Multi-Status (WebDAV)
-208 Already Reported (WebDAV)
-226 IM Used
-3xx Redirection
-300 Multiple Choices
-301 Moved Permanently
-302 Found
-303 See Other
- 304 Not Modified
-305 Use Proxy
-306 (Unused)
-307 Temporary Redirect
-308 Permanent Redirect (experimental)
-4xx Client Error
- 400 Bad Request
- 401 Unauthorized
-402 Payment Required
- 403 Forbidden
- 404 Not Found
-405 Method Not Allowed
-406 Not Acceptable
-407 Proxy Authentication Required
-408 Request Timeout
- 409 Conflict
-410 Gone
-411 Length Required
-412 Precondition Failed
-413 Request Entity Too Large
-414 Request-URI Too Long
-415 Unsupported Media Type
-416 Requested Range Not Satisfiable
-417 Expectation Failed
-418 I'm a teapot (RFC 2324)
-420 Enhance Your Calm (Twitter)
-422 Unprocessable Entity (WebDAV)
-423 Locked (WebDAV)
-424 Failed Dependency (WebDAV)
-425 Reserved for WebDAV
-426 Upgrade Required
-428 Precondition Required
-429 Too Many Requests
-431 Request Header Fields Too Large
-444 No Response (Nginx)
-449 Retry With (Microsoft)
-450 Blocked by Windows Parental Controls (Microsoft)
-451 Unavailable For Legal Reasons
-499 Client Closed Request (Nginx)
-5xx Server Error
- 500 Internal Server Error
-501 Not Implemented
-502 Bad Gateway
-503 Service Unavailable
-504 Gateway Timeout
-505 HTTP Version Not Supported
-506 Variant Also Negotiates (Experimental)
-507 Insufficient Storage (WebDAV)
-508 Loop Detected (WebDAV)
-509 Bandwidth Limit Exceeded (Apache)
-510 Not Extended
-511 Network Authentication Required"
-echo -e "$string" | grep "$site"
+    clear
+    
+    echo -e "Exemplo de como utilizar:\n\t./parse_url.sh meusite.com.br\n"
+    
+    exit 1
+    
+    }
+#
+#
+# Verifica a URL que foi obtida como paramentro($1),
+# e remove http's e www's caso existam, deixando a $url_limpa.
+#   Exemplo: https://google.com ~> google.com
+# 
+REMOVE_HTTP(){
+
+    echo "$1" | tr '[A-Z]' '[a-z]'
+
+    url_limpa=$(echo $1 | tr '[A-Z]' '[a-z]' | sed 's~http[s]*://~~g' | sed 's/www.//' |  cut -d "/" -f1)
+    
+    echo "SITE: $url_limpa"
+
 }
 
-if [ "$code" -ge "400" ];
+CODIGO _ERRO(){
+
+    code_erro=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $url_limpa)
+    
+    cat http_codes |  grep "$url_limpa"
+
+}
+
+if [ "$code_erro" -ge "400" ];
 then
-echo "HTTP_CODE: $code" 
-err $code
+echo "HTTP_CODE: $code_erro" 
+err $code_erro
 else
-echo "HTTP_CODE: $code" 
-err $code
-wget -q --show-progress --spider --recursive --no-verbose -O temp.html $site > /dev/null
+echo "HTTP_CODE: $code_erro" 
+err $code_erro
+wget -q --show-progress --spider --recursive --no-verbose -O temp.html $url_limpa > /dev/null
 grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*' temp.html | sort | uniq > temp1
 grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' temp.html >> temp1
 grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\:[0-9]\{1,5\}' temp.html >> temp1
@@ -132,8 +77,8 @@ cat temp1 | sort | uniq > parse.tmp
 links=$(cat parse.tmp | sort | uniq | wc -l)
 echo "Urls obtidas: $links"
 cat parse.tmp | uniq -c
-echo "#####################\n"
-file=$(echo $site | cut -d "." -f1)
+echo -e "#####################\n"
+file=$(echo $url_limpa | cut -d "." -f1)
 for host in $(cat parse.tmp | cut -d ':' -f2 | sed 's/www.//' | sed 's/\/\///' | cut -d '/' -f1 | sort -u ); do
 host $host  | grep -w "has\|NXDOMAIN\|mail" | sed 's/Host //' | sed 's/has address //' | sed 's/has IPv6 address //' >>  "$file"
 done
