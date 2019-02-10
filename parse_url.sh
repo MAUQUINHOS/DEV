ecfdd120be5db66f8f2e5d388c0c9587e1bd5669
MAUQUINHOS <@mauquinhos>
2019-02-10 00:22:49 -0200

    Script Filtando com comentarios

--- parse_url.sh
+++ parse_url.sh
@@ -3,7 +3,7 @@
 # parse_url.sh - Obtém URL's em um site
 #
 # Site  : linkedin.com/in/mauquinhos
-# Autor : Marcos R. Pereira
+# Autor : @MAUQUINHOS
 #
 # -----------------------------------------------------
 # Este programa recebe como um parametro um site e baixa sua index, e após
@@ -27,66 +27,94 @@
 #        accounts.google.com	:mail 
 #       ...
 #
-# Verifica os parametros existem para continuar o programa
-# Caso não tenha informa o modo de uso correto
-[ "$1" ] && {
+# -----------------------------------------------------
+#
+# Verifica a URL que foi obtida como paramentro($1) e se,
+# os parametros existem para continuar o programa
+# Caso não exista informa o modo  correto de uso.
+#   Exemplo: https://google.com ~> google.com 
+if [ "$1" == "" ];
+then 
 
     clear
     
-    echo -e "Exemplo de como utilizar:\n\t./parse_url.sh meusite.com.br\n"
+    echo -e "\n Exemplo de como utilizar:\n\t./parse_url.sh meusite.com.br\n"
     
     exit 1
     
-    }
+fi
 #
-#
-# Verifica a URL que foi obtida como paramentro($1),
-# e remove http's e www's caso existam, deixando a $url_limpa.
-#   Exemplo: https://google.com ~> google.com
-# 
+# Formata a URL com letras minúsculas para evitar erros, 
+# remove http's e www's caso existam e salva na variável  $url_limpa.
+
+clear
+
 REMOVE_HTTP(){
-
-    echo "$1" | tr '[A-Z]' '[a-z]'
-
+    
     url_limpa=$(echo $1 | tr '[A-Z]' '[a-z]' | sed 's~http[s]*://~~g' | sed 's/www.//' |  cut -d "/" -f1)
-    
-    echo "SITE: $url_limpa"
 
 }
 
-CODIGO _ERRO(){
+REMOVE_HTTP $1
 
-    code_erro=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $url_limpa)
-    
-    cat http_codes |  grep "$url_limpa"
+NOME_DO_ARQUIVO_GERADO=$(echo $url_limpa | cut -d'.' -f1) # Guarda o domínio para salvar o arquivo
 
-}
+echo "PARSING NO SITE: $url_limpa"
+#
+#
+# Obtém o cabeçalho do SITE e armazena o código HTTP_CODE na variavel $code_erro
+#
+code_erro=$(curl --silent --head --write-out '%{http_code}' $url_limpa | head -n1 | cut -d' ' -f2-6)
 
-if [ "$code_erro" -ge "400" ];
+echo "      HTTP_CODE: $code_erro" 
+
+code_erro=$(echo "$code_erro" | cut -d' ' -f1) 
+#
+#
+if [ "$code_erro" -ge "400" ]; # Verifica se o código é menor que 400 se for maior continua...
 then
-    echo "HTTP_CODE: $code_erro" 
-    err $code_erro
+
+    echo "         STATUS: SEM SUCESSO"
+
 else
-    echo "HTTP_CODE: $code_erro" 
-    err $code_erro
-    wget -q --show-progress --spider --recursive --no-verbose -O temp.html $url_limpa > /dev/null
-    grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*' temp.html | sort | uniq > temp1
-    grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' temp.html >> temp1
-    grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\:[0-9]\{1,5\}' temp.html >> temp1
-    cat temp1 | sort | uniq > parse.tmp
-    links=$(cat parse.tmp | sort | uniq | wc -l)
-    echo "Urls obtidas: $links"
-    cat parse.tmp | uniq -c
-    echo -e "#####################\n"
-    file=$(echo $url_limpa | cut -d "." -f1)
-    for host in $(cat parse.tmp | cut -d ':' -f2 | sed 's/www.//' | sed 's/\/\///' | cut -d '/' -f1 | sort -u ); do
-        host $host  | grep -w "has\|NXDOMAIN\|mail" | sed 's/Host //' | sed 's/has address //' | sed 's/has IPv6 address //' >>  "$file"
+
+    echo "         STATUS: INICIANDO DOWNLOAD"
+    wget -q -O $NOME_DO_ARQUIVO_GERADO $url_limpa # Baixando o site
+
+    echo "                 LINKS COM HTTP[s]"
+    grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*' $NOME_DO_ARQUIVO_GERADO > temporario
+
+    echo "                 LINKS COM IP[s]"
+    grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' $NOME_DO_ARQUIVO_GERADO >> temporario
+
+    echo "                 LINKS COM IP[s]:PORTAS"
+    grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\:[0-9]\{1,5\}' $NOME_DO_ARQUIVO_GERADO >> temporario
+
+    echo "                 $(cat temporario | wc -l) URL[s] NO TOTAL"
+
+    echo "                 $(cat temporario | sort -u | wc -l) URL[s] FILTRADAS"
+
+    echo "OBTENDO URL[s] E EXTRAINDO IP[s]: "
+
+    cat temporario | sort -u > parse.tmp
+
+    rm temporario
+
+    for URL in $(cat parse.tmp); do
+
+        REMOVE_HTTP $URL
+
+        host $url_limpa | grep -w "has\|NXDOMAIN\|mail" | sed 's/Host //' | sed 's/has address //' | sed 's/has IPv6 address //' >> temporario
+    
     done
-    links=$(cat $file | wc -l)
-    echo "Resolvendo URL's encontradas:"$links 
-    for host in $(cat "$file" | sort -u | cut -d " " -f 2,1  --output-delimiter='\t:'); do
-        echo -e "$host" | tr '\n' ' '
-        echo ""
-    done
-    rm temp.html temp1 parse.tmp
+
+    echo "==============================================="
+    
+    cat temporario  | cut -d" " -f1-2 | sort -u | grep -v "mail"
+    
+    echo "==============================================="
+    
+    mv temporario  "$NOME_DO_ARQUIVO_GERADO"
+    rm parse.tmp
+
 fi
