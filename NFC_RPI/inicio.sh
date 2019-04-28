dc0ea19dddbbfeb4aff9e78471b40682f05903a1
MAUQUINHOS <47393713+MAUQUINHOS@users.noreply.github.com>
2019-04-27 22:08:48 -0300

    Create inicio.sh

---
+++ NFC_RPI/inicio.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+###########################################################
+#
+#  Script desenvolvido para Obtenção de Graduação
+#  em Segurança da Informação na Fatec São Caetano do Sul
+#  com a Orientação do Prof: Ismael Parede 
+#
+###########################################################
+#
+#
+
