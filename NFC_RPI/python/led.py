1ea7db9062e7b8c5134e51e9e94c759b299de668
MAUQUINHOS <47393713+MAUQUINHOS@users.noreply.github.com>
2019-05-26 20:31:58 -0300

    Update led.py

--- NFC_RPI/python/led.py
+++ NFC_RPI/python/led.py
@@ -1,3 +1,15 @@
 # Referencias
 #
 #https://www.instructables.com/id/Control-LED-Using-Raspberry-Pi-GPIO/
+
+
+import RPi.GPIO as GPIO
+import time 
+GPIO.setmode(GPIO.BCM) 
+GPIO.setwarnings(False) 
+GPIO.setup(21,GPIO.OUT) 
+print "LED on" 
+GPIO.output(21,GPIO.HIGH) 
+time.sleep(10) 
+print "LED off" 
+GPIO.output(21,GPIO.LOW)
