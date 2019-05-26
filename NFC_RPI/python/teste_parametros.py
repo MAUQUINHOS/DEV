0209db50ba0c5504ad2d04fb249cceba44da0fb9
MAUQUINHOS <47393713+MAUQUINHOS@users.noreply.github.com>
2019-05-26 19:41:43 -0300

    Create teste_parametros.py

---
+++ NFC_RPI/python/teste_parametros.py
@@ -0,0 +1,24 @@
+#coding: utf-8
+#!/bin/python
+import sys
+from time import sleep
+#
+# laco de repetição
+# arg1 = 0.9 /segundos ou milesimos 
+# arg2 = 3   /numeros de voltas do laco 
+#
+# Funcionamento
+#
+# python teste_parametros.py arg1 arg2
+# python teste_parametros.py 0.3 1
+#
+# RecebeArgumentos
+sec=float(sys.argv[1])
+voltas=int(sys.argv[2])
+#Contador
+i=1
+#Imprime Laco
+while(i <= voltas ):
+	print(voltas)
+	sleep(sec)
+	i += 1
