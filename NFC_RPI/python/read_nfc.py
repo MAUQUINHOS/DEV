db1944d4ceb48df8c7a7f3f00847b7edfcb18c2b
MAUQUINHOS <47393713+MAUQUINHOS@users.noreply.github.com>
2019-06-03 17:11:03 -0300

    Update read_nfc.py

--- NFC_RPI/python/read_nfc.py
+++ NFC_RPI/python/read_nfc.py
@@ -1,17 +1,38 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Bibliotecas
-import struct
-import time
-import sys
+import struct,time, sys
+#
+##########################
+# MAPEAMENTO DAS TECLAS
+#    if code == 11:
+#        PASS += "0"
+#    elif code == 10:
+#        PASS += "9"
+#    elif code == 4 and value == 1:
+#        PASS += "3"
+#    elif code == 2:
+#        PASS += "1"
+#    elif code == 3:
+#        PASS += "2"
+#    elif code == 5:
+#        PASS += "4"
+#    elif code == 6:
+#        PASS += "5"
+#    elif code == 7:
+#        PASS += "6"
+#    elif code == 8:
+#        PASS += "7"
+#    elif code == 9:
+#        PASS += "8"
+##########################
 #
 # Codigo modificado
 # Original file: https://stackoverflow.com/questions/5060710/format-of-dev-input-event/10665053
 #
 # Para detectar o evento:  ls -l  /dev/input/by-{path,id}/ 
-# RESPOSTA: usb-Sycreader_RFID_Technology_Co.__Ltd_SYC_ID_IC_USB_Reader_08FF20140315-event-kbd -> ../event4
-
-# Caminho do arquivo
+# usb-Sycreader_RFID_Technology_Co.__Ltd_SYC_ID_IC_USB_Reader_08FF20140315-event-kbd -> ../event4
+# CAMINHO DA LEITURA
 infile_path = "/dev/input/event4"
 # Formata    o do STRUCT
 #long int, long int, unsigned short, unsigned short, unsigned int
@@ -25,39 +46,38 @@ in_file = open(infile_path, "rb")
 event = in_file.read(EVENT_SIZE)
 #
 # VARIAVEIS AMBIENTE
-IDNFC=""
+PASS=""
 #
 # APOS A LEITURA, CRIA UM LACO E TRADUZ O CODE
 while event:
         (tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
-        if code != 0 and value != 0:
-#                print("Event type %u, code %u, value %u at %d.%d" % (type, code, value, tv_sec, tv_usec))
+        if value != 0 and code != 0:
                 if code == 11:
-                        IDNFC += "0"
-                if code == 2:
-                        IDNFC += "1"
-                elif code == 3:
-                        IDNFC += "2"
+                        PASS += "0"
                 elif code == 10:
-                        IDNFC += "3"
+                        PASS += "9"
+                elif code == 4 and value == 1:
+                        PASS += "3"
+                elif code == 2:
+                        PASS += "1"
+                elif code == 3:
+                        PASS += "2"
                 elif code == 5:
-                        IDNFC += "4"
+                        PASS += "4"
                 elif code == 6:
-                        IDNFC += "5"
+                        PASS += "5"
                 elif code == 7:
-                        IDNFC += "6"
+                        PASS += "6"
                 elif code == 8:
-                        IDNFC += "7"
+                        PASS += "7"
                 elif code == 9:
-                        IDNFC += "8"
-                elif code == 28:
-                        IDNFC += "9"
+                        PASS += "8"
 # CHECA A SENHA E IMPRIME
-        if len(IDNFC) > 9:
-                print("ID: "+IDNFC)
-                IDNFC=""
+                if len(PASS) >= 10:
+                        print(PASS)
+                        PASS=""
         event = in_file.read(EVENT_SIZE)
+
 # FECHA O ARQUIVO
 in_file.close()
-
 
