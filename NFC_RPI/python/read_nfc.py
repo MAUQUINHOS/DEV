7702351d4680c965615f6e33723524d427383aba
MAUQUINHOS <47393713+MAUQUINHOS@users.noreply.github.com>
2019-06-03 12:50:30 -0300

    Update read_nfc.py

--- NFC_RPI/python/read_nfc.py
+++ NFC_RPI/python/read_nfc.py
@@ -11,7 +11,7 @@ import sys
 # Para detectar o evento:  ls -l  /dev/input/by-{path,id}/ 
 # RESPOSTA: usb-Sycreader_RFID_Technology_Co.__Ltd_SYC_ID_IC_USB_Reader_08FF20140315-event-kbd -> ../event4
 
-# Caminho do arquivoque ser   lido
+# Caminho do arquivo
 infile_path = "/dev/input/event4"
 # Formata    o do STRUCT
 #long int, long int, unsigned short, unsigned short, unsigned int
@@ -25,39 +25,39 @@ in_file = open(infile_path, "rb")
 event = in_file.read(EVENT_SIZE)
 #
 # VARIAVEIS AMBIENTE
-PASS=""
+IDNFC=""
 #
 # APOS A LEITURA, CRIA UM LACO E TRADUZ O CODE
 while event:
         (tv_sec, tv_usec, type, code, value) = struct.unpack(FORMAT, event)
         if code != 0 and value != 0:
-                print(event)
+#                print("Event type %u, code %u, value %u at %d.%d" % (type, code, value, tv_sec, tv_usec))
                 if code == 11:
-                        PASS += "0"
+                        IDNFC += "0"
                 if code == 2:
-                        PASS += "1"
+                        IDNFC += "1"
                 elif code == 3:
-                        PASS += "2"
+                        IDNFC += "2"
                 elif code == 10:
-                        PASS += "3"
-                        print("Event type %u, code %u, value %u at %d.%d" % (type, code, value, tv_sec, tv_usec))
+                        IDNFC += "3"
                 elif code == 5:
-                        PASS += "4"
+                        IDNFC += "4"
                 elif code == 6:
-                        PASS += "5"
+                        IDNFC += "5"
                 elif code == 7:
-                        PASS += "6"
+                        IDNFC += "6"
                 elif code == 8:
-                        PASS += "7"
+                        IDNFC += "7"
                 elif code == 9:
-                        PASS += "8"
+                        IDNFC += "8"
                 elif code == 28:
-                        PASS += "9"
-                        print("Event type %u, code %u, value %u at %d.%d" % (type, code, value, tv_sec, tv_usec))
+                        IDNFC += "9"
 # CHECA A SENHA E IMPRIME
-        if len(PASS) > 9:
-                print("ID: "+PASS)
-                PASS=""
+        if len(IDNFC) > 9:
+                print("ID: "+IDNFC)
+                IDNFC=""
         event = in_file.read(EVENT_SIZE)
 # FECHA O ARQUIVO
+in_file.close()
+
 
