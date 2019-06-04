a694a18ccca586657272936d33152eb8fa961310
MAUQUINHOS <47393713+MAUQUINHOS@users.noreply.github.com>
2019-06-04 07:37:36 -0300

    Update inicio.sh

--- NFC_RPI/inicio.sh
+++ NFC_RPI/inicio.sh
@@ -9,9 +9,6 @@
 ###########################################################
 #
 # Iniciando o servidor WEB
-SERVER_WEB=$(python -m server.py & echo $!)
+python -m server.py & echo -e "\tsudo kill -9 $!"
 # Ativa o Leitor NFC
-LEITOR_NFC=$(python read_nfc.py & echo $!)
-echo "Para matar os servicos"
-echo -e "\tsudo kill -9 $LEITOR_NFC"
-echo -e "\tsudo kill -9 $SERVER_WEB"
+python read_nfc.py & echo -e "\tsudo kill -9 $!"
