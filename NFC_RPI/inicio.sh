bdbfc61695144275f209fd823b8ccbe698fc1ad5
MAUQUINHOS <47393713+MAUQUINHOS@users.noreply.github.com>
2019-06-04 07:32:24 -0300

    Update inicio.sh

--- NFC_RPI/inicio.sh
+++ NFC_RPI/inicio.sh
@@ -4,9 +4,14 @@
 #
 #  Script desenvolvido para Obtenção de Graduação
 #  em Segurança da Informação na Fatec São Caetano do Sul
-#  com a Orientação do Prof: Ismael Parede 
+#  com a Orientação do Prof: Ismael Parede 2019
 #
 ###########################################################
 #
 # Iniciando o servidor WEB
-./mini_server.sh
+SERVER_WEB=$(python -m server.py & echo $!)
+# Ativa o Leitor NFC
+LEITOR_NFC=$(python read_nfc.py & echo $!)
+echo "Para matar os servicos"
+echo -e "\tsudo kill -9 $LEITOR_NFC"
+echo -e "\tsudo kill -9 $SERVER_WEB"
