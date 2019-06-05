6b82d22746dde80823197c3f671f279d3d5d5b55
MAUQUINHOS <47393713+MAUQUINHOS@users.noreply.github.com>
2019-06-05 16:10:20 -0300

    Update inicio.sh

--- NFC_RPI/inicio.sh
+++ NFC_RPI/inicio.sh
@@ -8,7 +8,17 @@
 #
 ###########################################################
 #
+# Limpando a tela
+clear
+#
+# Detectando EVENTO NFC e TECLADO
+EVENTO_NFC=$(ls -l  /dev/input/by-{path,id}/ | grep RFID | cut -d"/" -f2)
+EVENTO_NUMPAD=$(ls -l  /dev/input/by-{path,id}/ | grep "event-kbd" | grep -v "$EVENTO_NFC" | cut -d"/" -f2 | sort -u)
+echo -e "1- VERIFIQUE SE OS EVENTOS ESTAO CONFIGURADOS\n\n\t   NFC: $EVENTO_NFC \n\tNUMPAD: $EVENTO_NUMPAD\n"
+#
+echo -e "2- PARA FINALIZAR TAREFAS, USE:\n"
 # Iniciando o servidor WEB
 python -m server.py & echo -e "\tsudo kill -9 $!"
 # Ativa o Leitor NFC
-python read_nfc.py & echo -e "\tsudo kill -9 $!"
+python read_nfc.py $EVENTO_NFC & echo -e "\tsudo kill -9 $!"
+echo ""
