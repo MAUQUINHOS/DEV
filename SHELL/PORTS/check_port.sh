f124c01f0ea70e6cbe8d23e701322317ff1477e1
MAUQUINHOS <47393713+MAUQUINHOS@users.noreply.github.com>
2019-10-31 09:47:03 -0300

    Update functions
    Add Remote IP
    Add Comments

--- SHELL/PORTS/check_port.sh
+++ SHELL/PORTS/check_port.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
 #
-# Check Online Open Port using to site Can You See Me
+# Check Open Port using to site Can You See Me
+# Referece FORM POST:  https://ports.yougetsignal.com/check-port.php
 #
-# How to use: ./check_port.sh 8888
+# How to use
 #
-# BUFF for checking using IF
-BUFF=$(curl --data "port=$1" https://www.canyouseeme.org/ 2>&1 | grep "Success" | grep -o '<b>.*</b>' | sed 's/\(<b>\|<\/font>\|(\|<\/b>\)//g' | cut -d";" -f2)
+# For check a Port in Address use: ./check_port.sh PORT
+# ex: ./check_port.sh 8080
 #
-[[ ${#BUFF} -eq 0 ]] && { echo "Port: $1, is closed" >&2; exit 1; }
+# For check Port in Remote Address use: ./check_port.sh IP_REMOTE PORT
+# ex: ./check_port.sh 8.8.8.8 53
 #
-# Simple print output
-curl --data "port=$1" https://www.canyouseeme.org/ 2>&1 | grep "Success" | grep -o '<b>.*</b>' | sed 's/\(<b>\|<\/font>\|(\|<\/b>\)//g' | cut -d";" -f2
+# Check permissions before use.
+#
+# IF check  exists IP_REMOTE
+#
+# Autor: http://bit.ly/MAUQUINHOS-GIT
+# RAW  : https://raw.githubusercontent.com/MAUQUINHOS/DEV/master/SHELL/PORTS/check_port.sh
+# 
+clear
+echo -e "Check PORT ONLINE\n"
+if [ -z "$2" ]
+then
+# Check Port in your IP Gateway
+        curl -X POST -F "portNumber=$1" https://ports.yougetsignal.com/check-port.php 2>&1 | grep -qi "open" && echo -e "\tPORT: $1 OPEN" || echo -e "\tPORT: $1 CLOSED"
+
+else
+# Check Port in Remote IP
+        case `curl -X POST -F "remoteAddress=$1" -F "portNumber=$2" https://ports.yougetsignal.com/check-port.php 2>&1 | grep -qi "open"; echo $?` in
+          0)
+            echo -e "\tPort $2 in $1 OPEN"
+            ;;
+          1)
+            echo -e "\tPort $2 in $1 CLOSED"
+            ;;
+          *)
+            echo -e "\t Contais an ERROR!"
+            ;;
+        esac
+fi
+echo ""
